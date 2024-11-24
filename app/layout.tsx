b9240a95cb690bccd82afe5d014e9f99fc2847ee
fabiangzvo <faguzman.97@gmail.com>
2024-11-23 21:19:29 -0500

    customize next-ui theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import "@/styles/globals.css";
+import "@styles/globals.css";
+
 import { Metadata, Viewport } from "next";
 import { Link } from "@nextui-org/link";
 import clsx from "clsx";
 
-import { Providers } from "./providers";
+import { siteConfig } from "@config/site";
+import { fontSans } from "@config/fonts";
+import { Navbar } from "@components/navbar";
 
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: {
@@ -38,7 +39,7 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from "@nextui-org/theme";
+import colors from "tailwindcss/colors";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -16,6 +17,16 @@ module.exports = {
     },
   },
   darkMode: "class",
-  darkMode: "class",
- plugins: [nextui()],
-}
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: colors.emerald,
+            default: colors.emerald,
+          },
+        },
+      },
+    }),
+  ],
+};
