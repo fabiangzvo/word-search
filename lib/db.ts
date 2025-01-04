3ec51c0c39e50305997efabbe42ebbebf9c085c3
fabiangzvo <faguzman.97@gmail.com>
2025-01-04 15:29:14 -0500

    load models

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,22 @@
 import { MongoClient } from "mongodb";
 import mongoose from "mongoose";
 
+import * as models from "./models";
+
 let cached = global.mongoose;
+
+export async function registerModels(): Promise<void> {
+  for (const model of Object.values(models)) {
+    if (model) {
+      const modelName = model.modelName;
+
+      if (!mongoose.models[modelName]) {
+        mongoose.model(modelName, model.schema);
+        console.log(`Model registered: ${modelName}`);
+      }
+    }
+  }
+}
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
@@ -19,6 +34,8 @@ async function mongooseConnect(): Promise<typeof mongoose> {
   cached.promise = mongoose.connect(process.env.MONGODB_URI);
 
   cached.conn = await cached.promise;
+
+  await registerModels();
 
   return cached.conn;
 }
---
+++ lib/models/index.ts
@@ -0,0 +1,3 @@
+export { default as User } from "./user";
+export { default as Puzzle } from "./puzzle";
+export { default as Category } from "./category";
