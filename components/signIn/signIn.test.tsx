bfeca2dcca7a402aa9bbe3fd1f0ddeeb14706842
Fabián <faguzman.97@gmail.com>
2025-03-30 03:04:27 -0500

    fix login tests

--- components/signIn/signIn.test.tsx
+++ components/signIn/signIn.test.tsx
@@ -1,36 +1,109 @@
-import { it, describe, expect, vi, Mock } from 'vitest'
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { it, describe, expect, vi, beforeEach } from 'vitest';
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import type { Mock } from 'vitest';
 import { signIn } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 
-import SignIn from './index'
+import { handleSubmit } from '@/lib/actions/authentication'
+
+import SignIn from './index';
 
 vi.mock('next-auth/react', () => ({
   signIn: vi.fn(),
-}))
+}));
+vi.mock('next/navigation', () => ({
+  redirect: vi.fn(),
+}));
+vi.mock('@/lib/actions/authentication', { spy: true });
 
-global.alert = vi.fn()
-
-const mockSingIn = signIn as Mock
+const mockSignIn = signIn as Mock;
+const mockRedirect = redirect as unknown as Mock;
 
 describe('<SignIn />', () => {
-  it('debe mostrar un alert de error', async () => {
-    mockSingIn.mockResolvedValue({ error: 'Invalid credentials' })
+  beforeEach(() => {
+    mockSignIn.mockClear();
+    mockRedirect.mockClear();
+  });
 
-    render(<SignIn />)
+  it('Sign in should work', async () => {
+    mockSignIn.mockResolvedValue({
+      error: null,
+      status: 200,
+      ok: true,
+      url: '/dashboard',
+    });
 
-    const emailInput = screen.getByPlaceholderText('tu@email.com')
-    const passwordInput = screen.getByPlaceholderText('tu contraseña')
-    const button = screen.getByRole('button', { name: 'Ingresar' })
+    render(<SignIn />);
 
-    await act(() => {
-      fireEvent.change(emailInput, { target: { value: 'chupelo' } })
-      fireEvent.change(passwordInput, {
-        target: { value: 'camila chupa monda' },
+    const emailInput = screen.getByPlaceholderText('tu@email.com');
+    const passwordInput = screen.getByPlaceholderText('tu contraseña');
+    const button = screen.getByText('Ingresar');
+
+    await act(async () => {
+      fireEvent.change(emailInput, { target: { value: 'usuario@example.com' } });
+      fireEvent.change(passwordInput, { target: { value: 'contraseña123' } });
+      fireEvent.click(button);
+    });
+
+    await waitFor(() => {
+      expect(mockSignIn).toHaveBeenCalled();
+      expect(mockSignIn).toHaveBeenCalledWith('credentials', {
+        email: 'usuario@example.com',
+        password: 'contraseña123',
+        callbackUrl: '/dashboard',
+        redirect: false,
       })
+      expect(mockRedirect).toHaveBeenCalledWith('/dashboard')
+      expect(handleSubmit).toBeCalled()
+      expect(handleSubmit).toReturnWith(Promise.resolve(''))
+    });
+  });
 
-      fireEvent.click(button)
-    })
+  it('should show error message when fields are empty', async () => {
+    render(<SignIn />);
 
-    expect(screen.getByText('Invalid credentials')).toBeInTheDocument()
-  })
-})
+    const button = screen.getByText('Ingresar');
+
+    await act(async () => fireEvent.click(button));
+
+    await waitFor(() => {
+      const errorMessage = screen.getAllByText('Completa este campo')
+      expect(errorMessage[0]).toBeInTheDocument();
+      expect(errorMessage).toHaveLength(2)
+    });
+  });
+
+  it('should show loader, disable fields and button when is loading', async () => {
+    mockSignIn.mockImplementation(() => new Promise(resolve => setTimeout(resolve, 100)));
+
+    render(<SignIn />);
+
+    const emailInput = screen.getByPlaceholderText('tu@email.com');
+    const passwordInput = screen.getByPlaceholderText('tu contraseña');
+    const button = screen.getByText('Ingresar');
+
+    await act(async () => {
+      fireEvent.change(emailInput, { target: { value: 'usuario@example.com' } });
+      fireEvent.change(passwordInput, { target: { value: 'contraseña123' } });
+      fireEvent.click(button);
+    });
+    
+    await waitFor(() => {
+      expect(button).toHaveAttribute('data-loading', 'true');
+      expect(button.children).toHaveLength(2);
+      expect(button.children[0].ariaLabel).toBe('Loading');
+      expect(button).toBeDisabled();
+      expect(emailInput).toBeDisabled();
+      expect(passwordInput).toBeDisabled();
+    });
+
+    await waitFor(() => {
+      expect(button).not.toBeDisabled();
+      expect(button.getAttribute("data-loading")).not.toBe('true')
+      expect(emailInput).not.toBeDisabled();
+      expect(passwordInput).not.toBeDisabled();
+      expect(passwordInput).toBeEmptyDOMElement();
+      expect(emailInput).toBeEmptyDOMElement();
+    });
+  });
+});
