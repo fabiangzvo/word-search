5269b695c3a89a0e1ccda25ea9bdb8aa7001f710
fabiangzvo <faguzman.97@gmail.com>
2025-04-18 13:47:40 -0500

    change bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
-import { heroui } from "@heroui/theme";
-import colors from "tailwindcss/colors";
+import { heroui } from '@heroui/theme'
+import colors from 'tailwindcss/colors'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
-        kanit: ["var(--font-kanit)"],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
+        kanit: ['var(--font-kanit)'],
       },
       gridTemplateColumns: {
-        cards: "repeat(auto-fit, minmax(290px, 1fr))",
+        cards: 'repeat(auto-fit, minmax(290px, 1fr))',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [
     heroui({
       themes: {
@@ -28,17 +28,17 @@ module.exports = {
           colors: {
             primary: { ...colors.lime, DEFAULT: colors.lime[400] },
             default: { ...colors.lime, DEFAULT: colors.lime[400] },
-            background:colors.gray[50]
+            background: colors.gray[100],
           },
         },
         dark: {
           colors: {
             primary: { ...colors.fuchsia, DEFAULT: colors.fuchsia[500] },
             default: { ...colors.fuchsia, DEFAULT: colors.fuchsia[500] },
-            background:colors.zinc[950]
+            background: colors.zinc[950],
           },
         },
       },
     }),
   ],
-};
+}
