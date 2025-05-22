8a9ae8eb48ffa02b9f98de744b0bf9a852251ad7
fabiangzvo <faguzman.97@gmail.com>
2025-05-22 15:40:51 -0500

    fix dark styles

--- components/searchInput/index.tsx
+++ components/searchInput/index.tsx
@@ -1,18 +1,24 @@
 import { type JSX } from 'react'
 import { Kbd } from '@heroui/kbd'
-import { Input } from '@heroui/input'
+import { Input, InputProps } from '@heroui/input'
 import { SearchIcon } from 'lucide-react'
 
-function SearchInput(): JSX.Element {
+interface SearchInputProps {
+  variant?: InputProps['variant']
+}
+
+function SearchInput({ variant }: SearchInputProps): JSX.Element {
   return (
     <Input
+      variant={variant}
       aria-label="Search"
       classNames={{
         inputWrapper:
-          'bg-default-100 dark:bg-opacity-10 hover:bg-opacity-20 dark:focus:bg-opacity-30',
+          !variant &&
+          'bg-default-100 dark:bg-opacity-10 hover:bg-opacity-20 dark:group-data-[focus=true]:bg-opacity-10 dark:group-data-[hover=true]:bg-opacity-30',
         input: 'text-sm',
       }}
-      endContent={<Kbd className="hidden lg:inline-block" keys={['enter']} />}
+      endContent={<Kbd className="hidden lg:inline-block dark:bg-default-500 dark:bg-opacity-20 dark:text-default-500" keys={['enter']} />}
       labelPlacement="outside"
       placeholder="Search..."
       startContent={
