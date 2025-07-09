7a74df5287990dc8acce9bf0422cca98f3c5cebb
fabiangzvo <faguzman.97@gmail.com>
2025-07-09 14:27:43 -0500

    redirect sign-up when user is logged

--- middleware.ts
+++ middleware.ts
@@ -3,20 +3,27 @@ import { getToken } from 'next-auth/jwt'
 
 export async function middleware(req: NextRequest) {
   const isLogin = req.nextUrl.pathname === '/login'
+  const isSignUp = req.nextUrl.pathname === '/sign-up'
   const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
   const isPublicRoute = req.nextUrl.pathname.match(
     /\/(explore|puzzle\/[a-f0-9]{24}\/detail)/
   )
 
-  if (!session && !isLogin && !isPublicRoute)
+  if (!session && !isLogin && !isSignUp && !isPublicRoute)
     return NextResponse.redirect(new URL('/login', req.url))
 
-  if (session && isLogin && !isPublicRoute)
+  if (session && (isLogin || isSignUp) && !isPublicRoute)
     return NextResponse.redirect(new URL('/dashboard', req.url))
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/login', '/game/:path*', '/puzzle/:path*', '/dashboard/:path*'],
+  matcher: [
+    '/login',
+    '/sign-up',
+    '/game/:path*',
+    '/puzzle/:path*',
+    '/dashboard/:path*',
+  ],
 }
